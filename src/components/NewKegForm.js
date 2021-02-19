3382c8fbda5b4c9b42fc1cb370128977e56bb832
taylulz <taylulzcode@gmail.com>
2021-02-19 11:22:26 -0800

    attach reusable form to newKegForm

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
 
 export default function NewKegForm(props){
 
@@ -19,32 +20,9 @@ export default function NewKegForm(props){
 
   return (
     <>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input 
-          type='text'
-          name='name'
-          placeholder='Beverage Name' />
-        <input 
-          type='text'
-          name='brewery'
-          placeholder='Brewery' />
-          <textarea
-            name='description'
-            placeholde='Beverage Description' />
-        <input 
-          type='float'
-          name='abv'
-          placeholder='ABV number' />
-        <input 
-          type='float'
-          name='price'
-          placeholder='Price Per Pint' />
-        <input 
-          type='number'
-          name='pints'
-          placeholder='Enter 127 for New Keg' />
-        <button type='submit'>Add New Keg</button>
-      </form>
+      <ReusableForm 
+          buttonText="Add New Keg"
+          formSubmissionHandler={handleNewKegFormSubmission}/>
     </>
   );
 }
