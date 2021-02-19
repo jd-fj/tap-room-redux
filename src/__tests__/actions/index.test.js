428405a87a7f812e0673ee1f7176e020471edbd8
taylulz <taylulzcode@gmail.com>
2021-02-19 15:15:50 -0800

    fail TOGGLE_FORM test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,10 @@ describe('tap room actions', () => {
       id: 1
     });
   });
+
+  it('addKeg should create TOGGLE_FORM action',  () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 });
