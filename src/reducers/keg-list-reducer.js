e1353f4f8690389cf503dc4e665c5e031da9eb54
taylulz <taylulzcode@gmail.com>
2021-02-19 08:25:38 -0800

    purposefully pass second test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
+  const { name, brewery, abv, description, price, pints, id} = action;
+  switch(action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name,
+          brewery,
+          abv,
+          description,
+          price,
+          pints,
+          id
+        }
+      });
+      default:
         return state;
   }
+};
