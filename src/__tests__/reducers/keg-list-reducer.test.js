b68379378e9ed8fe4dd06092b4aeafc2c6b48741
taylulz <taylulzcode@gmail.com>
2021-02-19 08:24:33 -0800

    add second test, purposfully fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,49 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
-describe('kegListReducer', () =>{
+describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Goose Neck Pilsner',
+    brewery: 'Pillsbury Brewery',
+    abv: 6.7,
+    description: 'has a pill-y, light flavor, great for the whole family',
+    price: 10,
+    pints: 1,
+    id: "0"
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brewery, abv, description, price, pints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name,
+      brewery,
+      abv,
+      description,
+      price,
+      pints,
+      id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        brewery,
+        abv,
+        description,
+        price,
+        pints,
+        id
+      }
+    })
+  })
+
+
+
+
+
 });
