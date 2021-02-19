88a98d024db5ece2489381f3ca150787c28c8b75
taylulz <taylulzcode@gmail.com>
2021-02-19 15:10:46 -0800

    purposefully fail action test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,8 +1,10 @@
 import * as actions from './../../actions';
 
 describe('tap room actions', () => {
-  expect(actions.deleteKeg(1)).toEqual({
-    types: 'DELETE_KEG',
-    id: 1
+  it('deleteKeg should create DELETE_KEG action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: 'DELETE_KEG',
+      id: 1
+    });
   });
 });
