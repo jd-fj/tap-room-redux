2e24ab16fbdb283405002a2f9755591fc1d7e75d
taylulz <taylulzcode@gmail.com>
2021-02-19 15:25:01 -0800

    fail ADD_KEG test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -13,4 +13,25 @@ describe('tap room actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({
+      name: 'Goose Neck Pilsner',
+      brewery: 'Pillsbury Brewery',
+      abv: 6.7,
+      description: 'has a pill-y, light flavor, great for the whole family',
+      price: 10,
+      pints: 1,
+      id: 1
+    })).toEqual({
+      type: 'ADD_KEG',
+      name: 'Goose Neck Pilsner',
+      brewery: 'Pillsbury Brewery',
+      abv: 6.7,
+      description: 'has a pill-y, light flavor, great for the whole family',
+      price: 10,
+      pints: 1,
+      id: "0"
+    });
+  });
 });
