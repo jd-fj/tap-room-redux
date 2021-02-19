943c37cd569cce564c63f8e03cd0809980961d73
taylulz <taylulzcode@gmail.com>
2021-02-19 14:07:14 -0800

    purposefully pass test verifyng rootReducer wrks

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -7,4 +12,16 @@ describe('rootReducer', () => {
       formVisible: false
     });
   });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null}));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisible).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+
+
+
 });
